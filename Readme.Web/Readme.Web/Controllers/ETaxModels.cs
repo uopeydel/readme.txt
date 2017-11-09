4ec9d1ed0e489a8309d83f20791f3ada426d2258
uopeydel <lapadol@msn.com>
2017-11-09 11:44:42 +0700

    update

---
+++ Readme.Web/Readme.Web/Controllers/ETaxModels.cs
@@ -0,0 +1,32 @@
+﻿using SBXMLCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class TagAttributeModel
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+
+    public class XMLTagModel
+    {
+        public string Index { get; set; }
+        public string NameSpace { get; set; }
+        public string Name { get; set; }
+        public List<TagAttributeModel> Attribute { get; set; }
+        public string InnerText { get; set; }
+        public bool IsLeaf { get; set; }
+
+        public TElXMLDOMElement XmlElement { get; set; }
+    }
+
+
+    public class ETaxModels
+    {
+        public List<XMLTagModel> Data { get; set; }
+    }
+}
